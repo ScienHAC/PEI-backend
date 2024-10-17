194fabc6dbb3856d218b7c651b9e6b50ab7dce72
Piyush Sharma <90610178+ScienHAC@users.noreply.github.com>
2024-10-17 18:31:55 +0530

    "check final update on user controller"

--- Controllers/user.js
+++ Controllers/user.js
@@ -12,7 +12,7 @@ let handleUserSignup = async (req, res) => {
         const newUserDoc = await User.create({ name, email, password: hashedPassword, contact });
         const token = setUser(newUserDoc);
         res.cookie('_auth_token_pei', token, {
-            httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'None', maxAge: 7 * 24 * 60 * 60 * 1000
+            httpOnly: true, secure: true, sameSite: 'None', maxAge: 7 * 24 * 60 * 60 * 1000
         });
         console.log(newUserDoc);
         return res.status(201).json({ message: 'User created successfully', user: newUserDoc });
@@ -42,7 +42,7 @@ let handleUserLogin = async (req, res) => {
         const token = setUser(user);
 
         res.cookie('_auth_token_pei', token, {
-            httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'lax', maxAge: 7 * 24 * 60 * 60 * 1000
+            httpOnly: true, secure: true, sameSite: 'None', maxAge: 7 * 24 * 60 * 60 * 1000 //localhost put secure true is samesite none
         });
         const { password: _, ...userData } = user.toObject();
         console.log("Login Successfully");
